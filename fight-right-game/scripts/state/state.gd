3a193e84799aee439d05e06e7280c5e46c00da9c
Kobe300 <kobewatson14@gmail.com>
2024-07-28 01:23:20 -0500

    statemachine

---
+++ fight-right-game/scripts/state/state.gd
@@ -0,0 +1,18 @@
+extends Node
+
+
+# Called when the node enters a state.
+func _enter():
+	pass # Replace with function body.
+
+
+# Called when the node exit a state.
+func _exit(delta):
+	pass
+
+
+func _update():
+	pass
+
+func _physicsUpdate(delta):
+pass
