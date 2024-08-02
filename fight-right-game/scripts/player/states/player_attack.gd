66ba112dfd10d77711db4fa4c24a96fd2785e8ed
Kobe300 <kobewatson14@gmail.com>
2024-08-02 18:12:43 -0500

    state machne in works

--- fight-right-game/scripts/player/states/player_attack.gd
+++ fight-right-game/scripts/player/states/player_attack.gd
@@ -1,15 +1,20 @@
 #class_name Attack
 extends State
 
+@onready var player = $"../.."
+
 @export var ground_state: State
+
 # Called when the node enters a state.
 func enter() -> void:
+	#player.direction.x = 0.0
 	print('Character in Attack State')
+	print(player.direction.x)
 	#pass
 
 # Called when the node exit a state.
 func exit() -> void:
-	playback.travel("move")
+	#playback.travel("move")
 	print(" Exit Attack State")
 
 # Corrisponds with the _process() in "state_machine" script
@@ -19,13 +24,13 @@ func process_input(event: InputEvent):
 #Corrisponds with the _physics_process() in "state_machine" script
 func process_physics(delta: float):
 	#if(character.is_on_floor()):
-		#next_state = ground_state
+		#next_state = ground_state	
 		#pass
 	pass
+ 
 
-
-#func _on_animation_player_animation_finished(anim_name):
-	#if (anim_name == "attack_1"):
-		#next_state = ground_state
-		#print("attack animation played")
-		
+func _on_animation_tree_animation_finished(anim_name):
+	if(anim_name == "attack_1"):
+		next_state = ground_state
+	print(anim_name)
+	#pass # Replace with function body.
