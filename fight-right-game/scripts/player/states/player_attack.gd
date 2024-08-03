15657a555ecf73c1a990a6709298c6ee0545093f
Kobe300 <kobewatson14@gmail.com>
2024-08-03 10:01:49 -0500

    Update player_attack.gd

--- fight-right-game/scripts/player/states/player_attack.gd
+++ fight-right-game/scripts/player/states/player_attack.gd
@@ -1,15 +1,14 @@
 #class_name Attack
 extends State
 
-@onready var player = $"../.."
-
-@export var ground_state: State
+@export var ground_state : State
 
 # Called when the node enters a state.
 func enter():
 	playback.travel("attack_1")
 	print('Character in Attack State')
-	print(player.direction.x)
+	print(character.direction.x)
+	attack_face()
 	#pass
 
 # Called when the node exit a state.
@@ -23,9 +22,8 @@ func process_input(event: InputEvent):
 
 #Corrisponds with the _physics_process() in "state_machine" script
 func process_physics(delta: float):
-	player.direction = Input.get_vector("left", "right", "up", "down")
-	player.velocity.x = player.direction.x * 3000 * delta
-	#pass
+	print(character.facing_direction )
+	pass
  
 # Execute when animation is finished
 func _on_animation_tree_animation_finished(anim_name):
@@ -33,3 +31,11 @@ func _on_animation_tree_animation_finished(anim_name):
 		next_state = ground_state
 	print(anim_name)
 	#pass # Replace with function body.
+
+
+func attack_face():		
+	var new_position = character.global_position + Vector2(character.direction.x * 15, 0)  # Calculate the new position based on the direction and a speed factor (5 in this case)
+	character.global_position = new_position # Instantly move the character to the new position
+	#character.velocity.x = character.direction.x * 5
+	#character.move_local_x(character.velocity.x)
+	#character.move_and_slide()
