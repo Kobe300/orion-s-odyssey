34062ba08e736c60865b635bf295bdb04ad1131a
Kobe300 <kobewatson14@gmail.com>
2024-09-03 11:05:08 -0500

    fight animation improved

---
+++ fight-right-game/scripts/components/staminacomponent.gd
@@ -0,0 +1,19 @@
+extends Node2D
+class_name StaminaComponent
+
+@export var damage_taken_text : PackedScene
+@export var Max_stamina : float = 100
+var stamina : float
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	stamina = Max_stamina
+
+#
+func stamina_taken(energy: Stamina):
+	# Handles staminaLoss
+	stamina-= energy.energy_loss
+	
+	if stamina<= 0:
+		get_parent().queue_free()
+	print(str(get_parent()) + "stamina is " + str(stamina))
