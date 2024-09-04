63956bb7b5dd1cec85afe4f73d1ac6fec5ab633f
Kobe300 <kobewatson14@gmail.com>
2024-09-03 23:22:29 -0500

    Update attack_component.gd

--- fight-right-game/scripts/components/attack_component.gd
+++ fight-right-game/scripts/components/attack_component.gd
@@ -2,14 +2,20 @@ extends Area2D
 
 @export var attack_damage : float = 20
 @export var kick_damage : float = 30
+@export var state_machine : StateMachine 
+
 
 func _on_area_entered(area: Area2D) -> void:
 	print("Area entered:", area)
 	if area is HitBoxComponent:
 		var hitbox : HitBoxComponent = area
 		var attack = Attack.new()
-
-		attack.attack_damage = attack_damage
+	
+		if Input.is_action_just_pressed("kick"):
+			attack.attack_damage = kick_damage  # Use kick damage
+			
+		else:
+			attack.attack_damage = attack_damage
 		##attack.knock_back_force = knock_back_force
 		##attack.attack_position = global_position
 
