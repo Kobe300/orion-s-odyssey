48a61f56b0284984865efadb3d00b089d230faf8
Wayne102 <reachforevergreen@gmail.com>
2024-09-06 21:42:13 -0400

    Stamina bar Ui

--- fight-right-game/scripts/player/PlayerInfoUI.gd
+++ fight-right-game/scripts/player/PlayerInfoUI.gd
@@ -2,14 +2,21 @@ extends Control
 
 @onready var healthbar = $Health/Healthbar
 @onready var essencebar = $Essence/EssenceBar
+@onready var stamina_bar: TextureProgressBar = $Stamina/StaminaBar
 @export var health_component : HealthComponent
+@export var stamina_component : StaminaComponent
 
 func _ready():
 	healthbar.max_value = health_component.Max_health
 	healthbar.value = healthbar.max_value
 	print(healthbar.value)
+	
+	stamina_bar.max_value = stamina_component.Max_stamina
+	stamina_bar.value = stamina_bar.max_value
+	print(stamina_bar.value)
 
 func _process(delta):
 	healthbar.value = health_component.health
 	#manabar.value = PlayerInfo.mana
+	stamina_bar.value = stamina_component.stamina
 	
